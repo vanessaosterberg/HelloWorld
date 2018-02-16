b4031f398bb5623e911fabafdc48cca610a21123
Unknown <vanessa.osterberg@outlook.com>
2018-02-15 19:18:57 -0600

    My Name is...

--- src/TypingHelloWorld.java
+++ src/TypingHelloWorld.java
@@ -4,7 +4,8 @@ public class TypingHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World!");
+		System.out.println("Say Name!");
+		System.out.println("My name is Vanessa!");
 		
 		
 	}
